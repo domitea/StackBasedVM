a65ac0cd55c688060977aaae4ed5a632d840514a
Dominik Matoulek <domitea@gmail.com>
2014-01-17 19:52:35 +0100

    Added some functions

--- StackBasedVM/src/App/Virtual.java
+++ StackBasedVM/src/App/Virtual.java
@@ -37,13 +37,39 @@ public class Virtual {
 					int a = FIFO.pop();
 					FIFO.addFirst(a*b);
 				}
-				else if (arg.toUpperCase().equals("/"))
+				else if (arg.toUpperCase().equals("/")) 
 				{
 					int b = FIFO.pop();
 					int a = FIFO.pop();
 					FIFO.addFirst(a/b);
 				}
-				else if (arg.toUpperCase().equals("."))
+				else if (arg.toUpperCase().equals("SWAP")) // First -> Second and Second -> First
+				{
+					int b = FIFO.pop();
+					int a = FIFO.pop();
+					FIFO.addFirst(b);
+					FIFO.addFirst(a);
+				}
+				else if (arg.toUpperCase().equals("DUP")) // duplicate
+				{
+					int a = FIFO.pop();
+					FIFO.addFirst(a);
+					FIFO.addFirst(a);
+				}
+				else if (arg.toUpperCase().equals("SQRT")) // Square root
+				{
+					int a = FIFO.pop();
+					FIFO.addFirst((int) Math.round(Math.sqrt(a)));
+				}
+				else if (arg.toUpperCase().equals("CLEAR")) // Clear Stack
+				{
+					FIFO.clear();
+				}
+				else if (arg.toUpperCase().equals("PRINT")) // Print Stack
+				{
+					System.out.println(FIFO.toString());
+				}
+				else if (arg.toUpperCase().equals(".")) // Print first member of Stack
 				{
 					System.out.println(FIFO.getFirst());
 				}
